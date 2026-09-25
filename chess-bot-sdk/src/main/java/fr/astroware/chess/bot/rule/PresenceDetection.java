f86b05387d1e660f0b9807c2fdc235269215f678
Térence FERUT <46047190+AstrowareConception@users.noreply.github.com>
2026-09-25 09:59:51 +0200

    feat(bot-sdk): add PresenceDetection.java

---
+++ chess-bot-sdk/src/main/java/fr/astroware/chess/bot/rule/PresenceDetection.java
@@ -0,0 +1,9 @@
+package fr.astroware.chess.bot.rule;
+
+/**
+ * Détection minimale utilisée lorsqu'une situation n'a aucune information
+ * supplémentaire à transporter.
+ */
+public enum PresenceDetection implements Detection {
+    INSTANCE
+}
