388df902a9329a94d9e64753753f2c2e449808a8
Térence FERUT <46047190+AstrowareConception@users.noreply.github.com>
2026-09-25 14:20:53 +0200

    feat(position): add FileStatus.java

---
+++ chess-bot-sdk/src/main/java/fr/astroware/chess/bot/analysis/FileStatus.java
@@ -0,0 +1,11 @@
+package fr.astroware.chess.bot.analysis;
+
+/**
+ * Occupation d'une colonne par les pions.
+ */
+public enum FileStatus {
+    OPEN,
+    WHITE_SEMI_OPEN,
+    BLACK_SEMI_OPEN,
+    CLOSED
+}
